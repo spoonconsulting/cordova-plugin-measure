ae6c41d950556614195802eaf45d734f8da355e7
parveshneedhoo <parvesh.needhoo@spoonconsulting.com>
2024-08-27 17:54:00 +0400

    V1 - Spoon

--- src/ios/MeasurePlugin.swift
+++ src/ios/MeasurePlugin.swift
@@ -10,17 +10,14 @@ import SceneKit
     
     @objc func addARView(_ command: CDVInvokedUrlCommand) {
         DispatchQueue.global(qos: .utility).async {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            guard let myViewController = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController else {
-                fatalError("ViewController is not set in storyboard")
-            }
+            // Initialize the ViewController without using storyboard
+            let myViewController = ViewController()
             self.myViewController = myViewController
             self.myViewController.delegate = self
 
             DispatchQueue.main.async {
                 let options = command.arguments[0] as! NSMutableDictionary
-                let multiplePointsEnabled = options.object(forKey: "allowMultiplePoints") as! Bool;
-
+                let multiplePointsEnabled = options.object(forKey: "allowMultiplePoints") as! Bool
                 self.allowMultiplePoints = multiplePointsEnabled
 
                 guard let superview = self.webView.superview else { return }
